0f7ef9ff769ddcb4fa9e984ec3b4f06aa8ab6d46
oleksandrm <oleksandrm@interlink-ua.com>
2017-09-20 00:28:26 +0300

    Add routing and auth-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ContactDetailComponent } from './contact/contact-detail/contact-detail.component';
 import { ContactListComponent } from './contact/contact-list/contact-list.component';
+import { AuthComponent } from './auth/auth.component';
 import { BoardComponent } from './board/board.component';
+
+const routes: Routes = [
+  { path: '', component: BoardComponent, pathMatch: 'full' },
+  { path: 'login', component: AuthComponent },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     ContactDetailComponent,
     ContactListComponent,
+    AuthComponent,
     BoardComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(routes, { useHash: true })
   ],
   providers: [],
   bootstrap: [AppComponent]

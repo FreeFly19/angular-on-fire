7c8224ca43475e0ef7bcb4d29de8ee0d7fcbe443
oleksandrm <oleksandrm@interlink-ua.com>
2017-09-21 01:03:48 +0300

    [Demo] Add routing.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,26 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ContactDetailComponent } from './contact/contact-detail/contact-detail.component';
 import { ContactListComponent } from './contact/contact-list/contact-list.component';
 import { BoardComponent } from './board/board.component';
+import { AuthComponent } from './auth/auth.component';
 import { firebaseConfig } from '../firebase-config';
+
+const routes: Routes = [
+  { path: '', component: BoardComponent, pathMatch: 'full' },
+  { path: 'login', component: AuthComponent },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     ContactDetailComponent,
     ContactListComponent,
+    AuthComponent,
     BoardComponent
   ],
   imports: [
@@ -22,6 +30,7 @@ import { firebaseConfig } from '../firebase-config';
     FormsModule,
     AngularFireModule.initializeApp(firebaseConfig),
     AngularFireDatabaseModule,
+    RouterModule.forRoot(routes, { useHash: true })
   ],
   providers: [],
   bootstrap: [AppComponent]

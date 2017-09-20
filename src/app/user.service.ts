60d7b68886888ffbee96acbd0620f7945f5656dd
oleksandrm <oleksandrm@interlink-ua.com>
2017-09-21 02:26:31 +0300

    [Demo] Implement saving user to firebase functionality.

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -9,14 +9,20 @@ import * as firebase from 'firebase/app';
 import "rxjs/add/observable/of";
 import "rxjs/add/operator/mergeMap";
 import "rxjs/add/operator/map";
+import "rxjs/add/operator/first";
+
+import { User } from "./user.model";
+
 
 @Injectable()
 export class UserService {
-  readonly currentUser: Subject<firebase.User> = new BehaviorSubject<firebase.User>(null);
+  readonly currentUser: Subject<User> = new BehaviorSubject<User>(null);
 
   constructor(private afAuth: AngularFireAuth,
+              private afDatabase: AngularFireDatabase,
               router: Router) {
     afAuth.authState
+      .flatMap(user => user? this.getById(UserService.emailToId(user.email)): Observable.of(null))
       .subscribe(this.currentUser);
 
     this.currentUser
@@ -27,11 +33,34 @@ export class UserService {
   }
 
   signIn(): void {
-    this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+    this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider())
+      .then(authInfo => authInfo.user)
+      .then(user => this.update(user))
+      .then(user => this.currentUser.next(user));
   }
 
   signOut(): void {
     this.afAuth.auth.signOut();
   }
 
+  update(afUser: any): firebase.Promise<User> {
+    const user = {id: UserService.emailToId(afUser.email), displayName: afUser.displayName};
+
+    return this.afDatabase
+      .object(`/users/${user.id}`)
+      .update({displayName: user.displayName})
+      .then(() => user);
+  }
+
+  getById(userId: string): Observable<User> {
+    return this.afDatabase
+      .object(`/users/${userId}`)
+      .map(userSnapshot => {
+        return {id: userSnapshot.$key, displayName: userSnapshot.displayName};
+      });
+  }
+
+  private static emailToId(email: string): string {
+    return email.replace('.', ';');
+  }
 }

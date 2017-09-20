2d81bdd8c69cafecf05ece69b3281419c53f0ed1
oleksandrm <oleksandrm@interlink-ua.com>
2017-09-21 01:00:41 +0300

    Add auth component.

---
+++ src/app/auth/auth.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'auth-component',
+  templateUrl: './auth.component.html',
+  styleUrls: ['./auth.component.scss']
+})
+export class AuthComponent {
+  constructor() {
+  }
+}
